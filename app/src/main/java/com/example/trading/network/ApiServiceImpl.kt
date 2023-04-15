205f10a120e9540c0d0d3dec33172120b30dd604
rohilshahZC <rohil.shah@zoomcar.com>
2023-04-15 21:31:49 +0530

    string resource

--- app/src/main/java/com/example/trading/network/ApiServiceImpl.kt
+++ app/src/main/java/com/example/trading/network/ApiServiceImpl.kt
@@ -5,7 +5,6 @@ import io.ktor.client.*
 import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import kotlin.text.get
 
 class ApiServiceImpl(
     private val client: HttpClient
@@ -15,11 +14,8 @@ class ApiServiceImpl(
         return try {
             client.request {
                 method = HttpMethod.Get
-//                contentType(ContentType.Application.Json)
-
                 url(Endpoints.BASE_URL)
             }
-//            client.get { url(Endpoints.BASE_URL) }
         } catch (ex: RedirectResponseException) {
             // 3xx - responses
             println("Error: ${ex.response.status.description}")
--- app/src/main/java/com/example/trading/ui/view/ErrorView.kt
+++ app/src/main/java/com/example/trading/ui/view/ErrorView.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.trading.R
@@ -47,7 +48,7 @@ fun ErrorView(
 
             Text(
                 modifier = Modifier,
-                text = "Something Went wrong, \nPlease try again later"
+                text = stringResource(id = R.string.error_description)
             )
         }
     }
